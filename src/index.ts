7eff1914624a55eb8832b835ef30462a2cf2b16c
A90G <104040612+A90G@users.noreply.github.com>
2022-05-19 19:59:19 -0300

    commit falta prueba de escritorio

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
-import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+let num1: number = Number(prompt("ingrese un numero entero para buscar cantidad de divisores"));
+let num2: number = Number(prompt("ingrese un segundo numero entero como divisor"));
+
+
+function esMultiplo( num1: number , num2: number): boolean {
+  return ((num1 % num2 )=== 0);}
+  esMultiplo (num1, num2);
+
+function cantidadDeDivisores (num1: number) {
+  let cantidad: number = 0;
+for (let num2 = 0; num2 < num1; num2++) {
+  if (esMultiplo (num1, num2)){
+   cantidad++
+  }
+}
+return cantidad;
+}
+let numDeDivisores: number = cantidadDeDivisores(num1, num2);
+
+console.log ("El numero" , num1, "tiene", numDeDivisores, "divisores");
+
+
+/*necesito hacer prueba de escritorio*/
+
+
